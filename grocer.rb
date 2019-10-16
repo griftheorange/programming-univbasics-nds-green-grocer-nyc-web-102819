3ba85f5158420b11bb49d0894fef32fc149e3e29
Griffin Poole <griffinpoole5@gmail.com>
2019-10-16 02:22:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,9 +49,9 @@ end
 def apply_clearance(cart)
   cart.length.times {|i|
     if cart[i][:clearance]
-      cart[i][:price] *= 0.8
+      cart[i][:price] = (cart[i][:price]*0.8).round(2)
     end
-  }
+    cart
 end
 
 def checkout(cart, coupons)
